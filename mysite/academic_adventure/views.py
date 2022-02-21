0ec56f9128372a5ca4a97c3009bb6ab8bbc3d321
cameronstackhouse <stackhousecameron@gmail.com>
2022-02-21 22:42:52 +0000

    Added login functionality

--- mysite/academic_adventure/views.py
+++ mysite/academic_adventure/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required #Used to reject user entry to web page if not logged in
 
+@login_required
 def index(request):
     return HttpResponse("Hello")
 
+@login_required
 def home(request):
     return render(request, 'academic_adventure/home.html')
+
