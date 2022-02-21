8d010d06e09e79ad1d4c08c0dbd1ab1df6d15314
cameronstackhouse <stackhousecameron@gmail.com>
2022-02-21 20:35:57 +0000

    Added basic registration page that adds a valid user to DB of users

--- mysite/academic_adventure/views.py
+++ mysite/academic_adventure/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello World!")
+    return HttpResponse("Hello")
 
 def home(request):
-    return render(request, 'academic_adventure/home.html', )
+    return render(request, 'academic_adventure/home.html')
