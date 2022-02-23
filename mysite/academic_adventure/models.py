c05dd55ad9ffd71a226d05d15aecbcf9835a56a7
cameronstackhouse <stackhousecameron@gmail.com>
2022-02-23 01:21:00 +0000

    Added comments

--- mysite/academic_adventure/models.py
+++ mysite/academic_adventure/models.py
@@ -3,19 +3,24 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
+    """Defines a custom user that extends AbstractUser,
+    allowing for the built in Django authentication system
+    to be used with a specified user account."""
 
-    avatar = models.CharField(max_length=50)
-    intelligence = models.IntegerField(default=0)
-    sociability = models.IntegerField(default=0)
-    athleticism = models.IntegerField(default=0)
-    intel_xp = models.FloatField(default=0)
-    soc_xp = models.FloatField(default=0)
-    ath_xp = models.FloatField(default=0)
-    points = models.IntegerField(default=0)
-    gamekeeper = models.BooleanField(default=False)
+    avatar = models.CharField(max_length=50) #Link to avatar file that the user uses
+    intelligence = models.IntegerField(default=0) #Users intelligence score
+    sociability = models.IntegerField(default=0) #Users sociability score
+    athleticism = models.IntegerField(default=0) #Users athleticism score
+    intel_xp = models.FloatField(default=0) #Current xp for intelligence
+    soc_xp = models.FloatField(default=0) #Current xp for sociability
+    ath_xp = models.FloatField(default=0) #Current xp for athleticism
+    points = models.IntegerField(default=0) #Current user points
+    gamekeeper = models.BooleanField(default=False) #Boolean indicating if user is a gamekeeper or not
 
     @property
     def score(self):
+        """Function to calculate the users overall
+        score, using their attributes"""
         return (self.intelligence + self.sociability + self.athleticism) / 3
 
     def __str__(self):
