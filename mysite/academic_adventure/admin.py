8c96a17f11409e0402f78ad627e877e05975230d
cameronstackhouse <stackhousecameron@gmail.com>
2022-02-23 22:43:14 +0000

    Added society and event models

--- mysite/academic_adventure/admin.py
+++ mysite/academic_adventure/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Event, Society
+
+admin.site.register(CustomUser, UserAdmin)
+admin.site.register(Event)
+admin.site.register(Society)
--- mysite/academic_adventure/models.py
+++ mysite/academic_adventure/models.py
@@ -1,3 +1,5 @@
+from pyexpat import model
+from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -26,7 +28,26 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
+class Society(models.Model):
+    """Defines a society"""
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    members = models.ManyToManyField(CustomUser, blank=True)
 
+    def __str__(self):
+        return self.name
 
+class Event(models.Model):
+    """Defines an event in the database"""
+    longitude = models.DecimalField(decimal_places=20, max_digits=30)
+    latitude = models.DecimalField(decimal_places=20, max_digits=30)
+    name = models.CharField(max_length=100)
+    host = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    duration = models.DecimalField(decimal_places=2, max_digits=6)
+    code = models.CharField(max_length=100)
+    type = models.CharField(max_length=40)
+    society = models.OneToOneField(Society, on_delete=models.CASCADE, blank=True)
 
-
+    def __str__(self):
+        return self.name
