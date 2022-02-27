dd21fa801c16484c706244ee8afdc6164aed0138
cameronstackhouse <stackhousecameron@gmail.com>
2022-02-27 20:50:03 +0000

    Revert "Merge pull request #24 from cameronstackhouse/Navigation-Bar-and-Modal-interface-transition"
    This reverts commit 86b355ece29826ad3181071b9492bbf23bc0c2a6, reversing
    changes made to b00f9d174c4387191cf2e1c58017cd246a875309.

--- mysite/academic_adventure/urls.py
+++ mysite/academic_adventure/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', views.home, name='index'),
     path('home/', views.home, name='home'),
     path('map/', views.map, name='map'),
+    path('leaderboard/', views.leaderboard, name='leaderboard'),
+    path('scan/', views.scan, name='scan'),
     path('create/', views.create, name='create'),
     path('create/<int:event_id>/', views.code, name='code')
 ]
