dcf2b0f759e0ad8f5363949b214fc0f8ebb2b44e
cameronstackhouse <stackhousecameron@gmail.com>
2022-03-04 19:42:02 +0000

    Added some basic tests for views

--- mysite/academic_adventure/tests.py
+++ mysite/academic_adventure/tests.py
@@ -1,11 +1,17 @@
 import datetime
+from urllib import response
 
 from django.test import TestCase
+from django.test.client import Client
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Event
 
 class EventModelTest(TestCase):
+    """
+    Tests for the event model in the database
+    """
     def event_is_recent_with_future_event(self):
         """
         recent() returns False for events that are not within
@@ -46,5 +52,67 @@ class EventModelTest(TestCase):
         time = timezone.now() + datetime.timedelta(hours=2)
         current_event = Event(date = time)
 
-        self.assertIs(current_event.recent(), True)    
+        self.assertIs(current_event.recent(), True)   
+
+
+class LoginTest(TestCase):
+    """
+    Tests for the login view
+    """
+
+    def test_login_GET(self):
+        """
+        Test the login view when the user is not logged in
+        """
+        client = Client()
+        response = client.get("/academic-adventure/login/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/login.html")
+
+    def test_login_success(self):
+        """
+        Test the login view when the user is logged in.
+        Should redirect the user to the home page.
+        """
+        pass
+
+
+class HomeViewTest(TestCase):
+    """
+    Tests for the home view
+    """
+    
+    def test_home_GET(self):
+        client = Client()
+        response = client.get(reverse("academic_adventure:home"), follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("home.html")
+
+
+class MapViewTest(TestCase):
+    """
+    Tests for the map view
+    """
+
+    def test_map_GET(self):
+        client = Client()
+        response = client.get(reverse("academic_adventure:map"), follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("map.html")
+
+
+class LeaderboardViewTest(TestCase):
+    """
+    Tests for the leaderboard view
+    """
+
+    def test_leaderboard_GET(self):
+        client = Client()
+        response = client.get(reverse("academic_adventure:leaderboard"), follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("leaderboard.html")
 
