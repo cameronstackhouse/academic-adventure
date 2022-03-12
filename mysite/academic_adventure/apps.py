87a5116be130da2b80749e07b9c8542ca47dd3b8
cameronstackhouse <stackhousecameron@gmail.com>
2022-03-12 14:05:28 +0000

    Comments added to apps.py

--- mysite/academic_adventure/apps.py
+++ mysite/academic_adventure/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class AcademicAdventureConfig(AppConfig):
+    """
+    Config for the academic adventure app
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'academic_adventure'
