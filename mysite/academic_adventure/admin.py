082bd217816806892a0d5d357ae16e96bcca1083
cameronstackhouse <stackhousecameron@gmail.com>
2022-03-19 12:32:20 +0000

    added ability to set gamekeepers through admin.py

--- mysite/academic_adventure/admin.py
+++ mysite/academic_adventure/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Event, Society
 
-admin.site.register(CustomUser, UserAdmin) #Registers custom user with admin
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (
+            'Custom user fields',
+            {
+                'fields':(
+                    "gamekeeper",
+                ),
+            },
+        ),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin) #Registers custom user with admin
 admin.site.register(Event) #Registers event with admin
 admin.site.register(Society) #Registers society with admin
