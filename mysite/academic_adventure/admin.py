cfe208c6591180bb549e2c8ad32549c5f5b46810
cameronstackhouse <stackhousecameron@gmail.com>
2022-03-19 13:27:21 +0000

    Added code comments to custom user admin

--- mysite/academic_adventure/admin.py
+++ mysite/academic_adventure/admin.py
@@ -3,13 +3,19 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Event, Society
 
 class CustomUserAdmin(UserAdmin):
+    """
+    Custom user admin to allow for the extended user attributes
+    to be displayed on the admin.py page.
+    """
+    #Fields in the creation form
     fieldsets = (
-        *UserAdmin.fieldsets,
+        *UserAdmin.fieldsets, #Fields in the default user admin page
         (
             'Custom user fields',
             {
                 'fields':(
-                    "gamekeeper",
+                    "gamekeeper", #Field to determine if a user is a gamekeeper
+                    #Can be used to set gamekeepers in the system
                 ),
             },
         ),
